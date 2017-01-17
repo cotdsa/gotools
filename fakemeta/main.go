0b7c3e33946b54207c4c1258af1254e0d6464852
Matteo Sessa <matteo.sessa@catchoftheday.com.au>
2017-01-17 12:06:47 +1100

    Stable synchronous

--- fakemeta/main.go
+++ fakemeta/main.go
@@ -6,10 +6,9 @@ import (
 	"github.com/hashicorp/vault/api"
 	"net/http"
 	"time"
-	//"os"
 )
 
-type IAMResponse struct {
+type IAMResponseType struct {
 	Code            string
 	LastUpdated     string
 	Type            string
@@ -17,6 +16,19 @@ type IAMResponse struct {
 	SecretAccessKey string
 	Token           string
 	Expiration      string
+}
+
+type CacheType struct {
+	UserToken         string
+	UserTokenExpiry   time.Time
+	IAMResponse       IAMResponseType
+	IAMResponseExpiry time.Time
+}
+
+type appContext struct {
+	currentProfile string
+	currentPath    string
+	cache          CacheType
 }
 
 func authenticate_userpass(c *api.Client, user string, pass string) (string, time.Time, error) {
@@ -54,12 +66,13 @@ func authenticate_userpass(c *api.Client, user string, pass string) (string, tim
 	auth := body["auth"].(map[string]interface{})
 	token = auth["client_token"].(string)
 
-	expiry = time.Now().Local().Add(time.Second * time.Duration(auth["lease_duration"].(float64)))
+	//expiry = time.Now().Add(time.Second * time.Duration(auth["lease_duration"].(float64)))
+	expiry = time.Now().Add(time.Second * time.Duration(30))
 
 	return token, expiry, nil
 }
 
-func get_iam_creds(c *api.Client, rolename string) (string, string, string, time.Time, error) {
+func get_vault_iam_creds(c *api.Client, path string) (string, string, string, time.Time, error) {
 	var (
 		access_key     string
 		secret_key     string
@@ -69,75 +82,94 @@ func get_iam_creds(c *api.Client, rolename string) (string, string, string, time
 	)
 
 	l := c.Logical()
-	s, err := l.Read(fmt.Sprintf("awstest/sts/%s", rolename))
+	s, err := l.Read(path)
 	if err != nil {
 		return access_key, secret_key, security_token, expiry, err
 	}
-	for key, value := range s.Data {
-		fmt.Printf("%s\t%v\n", key, value)
-	}
+
 	access_key = s.Data["access_key"].(string)
 	secret_key = s.Data["secret_key"].(string)
 	security_token = s.Data["security_token"].(string)
-	expiry = time.Now().Local().Add(time.Second * time.Duration(s.LeaseDuration))
+	//expiry = time.Now().Add(time.Second * time.Duration(s.LeaseDuration))
+	expiry = time.Now().Add(time.Second * time.Duration(10))
 
 	return access_key, secret_key, security_token, expiry, nil
 
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (ah *appContext) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		access_key     string
+		secret_key     string
+		security_token string
+		err            error
+	)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	requested_profile := r.URL.Path[len("/latest/meta-data/iam/security-credentials/"):]
+
+	if requested_profile == "" {
+		fmt.Fprintf(w, "%s", ah.currentProfile)
+		return
+	}
+
+	//f, _ := w.(http.Flusher)
+	//f.Flush()
 
 	config := api.DefaultConfig()
 	config.Address = "https://vault.cgws.com.au"
 	c, _ := api.NewClient(config)
 
-	//if err != nil {
-	//	panic(err)
-	//}
-	vault_username, vault_password, err := get_user_password_interactive()
+	if time.Now().After(ah.cache.UserTokenExpiry) {
+
+		vault_username, vault_password, err := get_user_password_interactive()
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+
+		ah.cache.UserToken, ah.cache.UserTokenExpiry, err = authenticate_userpass(c, vault_username, vault_password)
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+			return
+		}
+	}
+	c.SetToken(ah.cache.UserToken)
+
+	if time.Now().After(ah.cache.IAMResponseExpiry) {
+		access_key, secret_key, security_token, ah.cache.IAMResponseExpiry, err = get_vault_iam_creds(c, ah.currentPath+"powerusersts")
+
+		ah.cache.IAMResponse = IAMResponseType{
+			Code:            "Success",
+			LastUpdated:     time.Now().Format("2006-01-02T15:04:05Z"),
+			Type:            "AWS-HMAC",
+			AccessKeyId:     access_key,
+			SecretAccessKey: secret_key,
+			Token:           security_token,
+			Expiration:      ah.cache.IAMResponseExpiry.Format("2006-01-02T15:04:05Z"),
+		}
+	}
+
+	js, err := json.Marshal(ah.cache.IAMResponse)
 	if err != nil {
 		fmt.Fprintf(w, "%v", err)
 		return
 	}
 
-	token, _, err := authenticate_userpass(c, vault_username, vault_password)
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-
-	c.SetToken(token)
-
-	access_key, secret_key, security_token, exipry, err := get_iam_creds(c, "powerusersts")
-
-	iamresponse := IAMResponse{
-		Code:            "Success",
-		LastUpdated:     time.Now().Local().Format("2006-01-02T15:04:05Z"),
-		Type:            "AWS-HMAC",
-		AccessKeyId:     access_key,
-		SecretAccessKey: secret_key,
-		Token:           security_token,
-		Expiration:      exipry.Format("2006-01-02T15:04:05Z"),
-	}
-
-	js, err := json.Marshal(iamresponse)
-	if err != nil {
-		fmt.Fprintf(w, "%v", err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
-	//fmt.Fprintf(w, "Hi there %s, Your vault token is %s and will expire at %s", vault_username, token, expiry)
-	//os.Exit(0)
 }
 
 func main() {
-	t := time.Now()
-	fmt.Println(t.Format("2012-04-26T16:39:16Z"))
-	http.HandleFunc("/latest/meta-data/iam/security-credentials/s3access", handler)
-	//a, b, err := get_user_password_interactive()
-	//fmt.Printf("user=%s, pass=%s", a, b)
+
+	context := &appContext{
+		currentProfile: "s3access",
+		currentPath:    "awstest/sts/",
+		cache:          CacheType{},
+	}
+
+	http.Handle("/latest/meta-data/iam/security-credentials/", context)
 	err := http.ListenAndServe("169.254.169.254:80", nil)
 	fmt.Println(err)
 }
